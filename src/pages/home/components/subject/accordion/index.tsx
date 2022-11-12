a0c90299a1e2ab358e8dee18a6dbbe7033d642b4
zantaclaus <tarzuto@gmail.com>
2022-11-12 14:03:01 +0700

    Implement AccordionContent

--- src/pages/home/components/subject/accordion/index.tsx
+++ src/pages/home/components/subject/accordion/index.tsx
@@ -3,12 +3,12 @@ import AccordionContent from './AccordionContent';
 
 const SubjectAccordion = () => {
   return (
-    <div className='px-5'>
+    <div className='px-5 h-full overflow-auto'>
       <header className='pt-6 mb-3'>
         <h1 className=' text-lg sukhumvit-semibold text-white'>วิชาเรีียนของฉัน</h1>
       </header>
 
-      <div className='text-white h-[calc(100%-4rem)]'>
+      <div className='text-white h-[calc(100%-4rem)] overflow-auto accordion'>
         <AccordionContent />
       </div>
     </div>
