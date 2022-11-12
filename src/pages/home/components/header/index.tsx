b76f3bf03eaca70ce72b6ab8d98e1bfb50ad9425
zantaclaus <tarzuto@gmail.com>
2022-11-12 11:10:08 +0700

    Implement HomeHeader

---
+++ src/pages/home/components/header/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { FaUserCircle } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
+const HomeHeader = () => {
+  return (
+    <header className='px-6 flex justify-between'>
+      <div></div>
+      <Link to='/profile'>
+        <FaUserCircle className='text-white text-2xl' />
+      </Link>
+    </header>
+  );
+};
+
+export default HomeHeader;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import HomeHeader from './components/header';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className='w-screen h-screen py-6 bg-background grid grid-rows-[40px_40px_40fr_60fr]'>
+      <HomeHeader />
+      <div className='w-full h-full bg-red-300'></div>
+      <div className='w-full h-full bg-green-300'></div>
+      <div className='w-full h-full bg-orange-300'></div>
+    </div>
+  );
 };
 
 export default Home;
