c812bd1dbfe6b9588b4f6d354f73d072fe873bca
zantaclaus <tarzuto@gmail.com>
2022-11-12 18:25:30 +0700

    Implement AuthProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import Routes from 'routes';
+import { LiffProvider } from 'react-liff';
 import { BrowserRouter } from 'react-router-dom';
 import SubjectProvider from 'contexts/subject/SubjectProvider';
 import { ToastProvider } from 'contexts/toast/ToastProvider';
 import Toast from 'components/common/Toast';
+import AuthProvider from 'contexts/auth/AuthProvider';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <SubjectProvider>
-        <ToastProvider>
-          <Routes />
-          <Toast />
-        </ToastProvider>
-      </SubjectProvider>
-    </BrowserRouter>
+    <LiffProvider liffId='1657631189-9wk8vxJA'>
+      <BrowserRouter>
+        <AuthProvider>
+          <SubjectProvider>
+            <ToastProvider>
+              <Routes />
+              <Toast />
+            </ToastProvider>
+          </SubjectProvider>
+        </AuthProvider>
+      </BrowserRouter>
+    </LiffProvider>
   );
 };
 
