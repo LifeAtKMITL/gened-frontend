3314ea8ed4d291b01f52e7a8361b50ea9433449b
zantaclaus <tarzuto@gmail.com>
2022-11-12 10:44:18 +0700

    Add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const App: React.FC = (): JSX.Element => {
-  return <div></div>;
+const App = () => {
+  return <div className='w-screen h-screen bg-blue-300'></div>;
 };
 
 export default App;
