550a03ea5d4efc1d0922a5b865c30da168dfe922
zantaclaus <tarzuto@gmail.com>
2022-11-12 12:12:14 +0700

    Implement DropdownSerach

--- src/pages/home/components/subject/drawer/AddSubjectForm.tsx
+++ src/pages/home/components/subject/drawer/AddSubjectForm.tsx
@@ -1,7 +1,27 @@
+import { DropdownSearch } from 'components/inputs';
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { transformOptions } from 'services/subject';
 
 const AddSubjectForm = () => {
-  return <div></div>;
+  const {
+    control,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const options = {};
+
+  return (
+    <div>
+      <DropdownSearch
+        control={control}
+        name='subject'
+        label='ค้นหาจากชื่อหรือรหัสวิชา'
+        options={transformOptions(options as any)}
+        error={errors['subject']}
+      />
+    </div>
+  );
 };
 
 export default AddSubjectForm;
