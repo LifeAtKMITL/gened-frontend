5f600daa282950a3e24d60e7307f274814fc644e
zantaclaus <tarzuto@gmail.com>
2022-11-12 17:11:28 +0700

    Implement ToastContext

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import Routes from 'routes';
 import { BrowserRouter } from 'react-router-dom';
 import SubjectProvider from 'contexts/subject/SubjectProvider';
+import { ToastProvider } from 'contexts/toast/ToastProvider';
 
 const App = () => {
   return (
     <BrowserRouter>
       <SubjectProvider>
-        <Routes />
+        <ToastProvider>
+          <Routes />
+        </ToastProvider>
       </SubjectProvider>
     </BrowserRouter>
   );
---
+++ src/contexts/toast/ToastProvider.tsx
@@ -0,0 +1,26 @@
+import React, { useReducer } from 'react';
+import { IChildren } from 'types/component';
+import { IToastState, ToastType } from 'types/toast';
+import { ToastContext } from './ToastContext';
+import { toastReducer } from './toastReducer';
+
+const initialState: IToastState = {
+  open: false,
+  text: '',
+  type: 'success',
+};
+
+export const ToastProvider: React.FC<IChildren> = ({ children }) => {
+  const [toastState, dispatch] = useReducer(toastReducer, initialState);
+
+  const toggleToast = (open: boolean, text: string = '', type: ToastType = 'success') => {
+    if (!open) {
+      dispatch({ type: 'close' });
+      return;
+    }
+
+    dispatch({ type: 'open', payload: { text, type } });
+  };
+
+  return <ToastContext.Provider value={{ ...toastState, toggleToast }}>{children}</ToastContext.Provider>;
+};
