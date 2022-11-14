b022ebe5a40f8361fe2e36a952b5e1e3a9f70d81
zantaclaus <tarzuto@gmail.com>
2022-11-14 14:09:58 +0700

    🛠 Add navbar - logo

--- src/pages/home/components/header/index.tsx
+++ src/pages/home/components/header/index.tsx
@@ -4,8 +4,10 @@ import { Link } from 'react-router-dom';
 
 const HomeHeader = () => {
   return (
-    <header className='px-6 flex justify-between mb-4'>
-      <div></div>
+    <header className='px-6 flex justify-between items-center mb-4'>
+      <h1 className='text-white sukhumvit-bold text-xl'>
+        LIFE@<span className='text-primary sukhumvit-bold'>KMITL</span>
+      </h1>
       <Link to='/profile'>
         <FaUserCircle className='text-white text-2xl' />
       </Link>
